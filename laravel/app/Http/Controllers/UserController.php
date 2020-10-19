9c0b06dd51cca7ccec256d0972baaa2c33643e80
kazunari0220 <67147883+kazunari0220@users.noreply.github.com>
2020-10-19 22:29:48 +0900

    heroku

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -20,7 +20,8 @@ class UserController extends Controller
 
     public function likes(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load(['likes.user', 'likes.likes', 'likes.tags']);
 
         $articles = $user->likes->sortByDesc('created_at');
 
@@ -32,9 +33,11 @@ class UserController extends Controller
 
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load('followings.followers');
 
-        $followings = $user->followings->sortByDesc('created_at');
+        $followings = $user->followings->sortByDesc('created_at')
+        ->load(['articles.user', 'articles.likes', 'articles.tags']);
 
         return view('users.followings', [
             'user' => $user,
@@ -44,7 +47,8 @@ class UserController extends Controller
     
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+        ->load('followers.followers');
 
         $followers = $user->followers->sortByDesc('created_at');
 
